4db03575f221ac0eaefddf653746e51c69900a5c
Dmitriy Dorosh <doroshdmitriy@gmail.com>
2020-01-15 17:56:45 -0800

    first commit

---
+++ Assets/HelloWorld.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    int min;
+    int max;
+    int guess;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        min = 1;
+        max = 1000;
+        guess = 500;
+        Debug.Log("Welcome to Number Wizard");
+        Debug.Log("I would like you to pick a number");
+        Debug.Log($"Pick between {min} and {max}");
+        Debug.Log("And only whole numbers you doorknob");
+        Debug.Log("Tell me if your number is higher or lower than 500");
+        Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
+        max += 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = guess;
+            NextGuess();
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = guess;
+            NextGuess();
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Got it!");
+            StartGame();
+        }
+    }
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        Debug.Log($"Is it higher or lower than {guess}");
+    }
+}
